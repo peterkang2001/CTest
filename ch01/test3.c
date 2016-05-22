fa0e7214768ecc997bcbf6f2f747b6bd3c1b8b00
peterkang2001 <peterkang2001@gmail.com>
2016-05-22 13:05:08 +0800

    add test3.c

---
+++ ch01/test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Hi there!\n\n\nThis program is a bit");
+	printf(" longer than the others.");
+	printf("\nBut really it's only more text.\n\n\n\a\a");
+	printf("Hey, wait a minute!! What was that???\n\n");
+	printf("\t1.\tA bird?\n");
+	printf("\t2.\tA plane?\n");
+	printf("\t3.\tA control charater?\n");
+	printf("\n\t\t\b\bAnd how will this look when it prints out?\n");
+	return 0;
+}
