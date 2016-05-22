13457ed140e6e1130e694caf6d9192f189a876f5
peterkang2001 <peterkang2001@gmail.com>
2016-05-22 13:37:19 +0800

    add ch02

---
+++ ch02/test1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("My salary is $10000\n");
+	return 0;
+}
---
+++ ch02/test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int sarlary;
+	sarlary = 10000;
+	printf("My salary is %d\n", sarlary);
+	return 0;
+}
